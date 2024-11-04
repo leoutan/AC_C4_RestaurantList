9885c1ed433a5f94f836329281fca1ed8578b8da
Leo Tseng <leo81xi16@gmail.com>
2024-11-04 14:48:29 +0800

    implement routers

--- app.js
+++ app.js
@@ -13,6 +13,37 @@ app.get('/', (req, res)=>{
   res.render('index')
 })
 
+app.get('/restaurants', (req, res)=>{
+  res.render('restaurants')
+})
+
+app.get('/restaurants/:id', (req, res)=>{
+  const id = req.params.id
+  res.render('restaurant', {id})
+})
+
+app.get('/restaurants/new', (req, res)=>{
+  res.render('new')
+})
+
+app.get('/restaurants/:id/edit', (req, res)=>{
+  const id = req.params.id
+  res.render('edit', {id})
+})
+
+app.post('/restaurants', (req, res)=>{
+  res.send('新增餐廳')
+})
+
+app.put('/restaurants/:id', (req, res)=>{
+  res.send('編輯餐廳')
+})
+
+app.delete('/restaurants/:id', (req, res)=>{
+  res.send('刪除餐廳')
+})
+
+
 app.listen(port, ()=>{
   console.log(`server is working on http://localhost:${port}`)
 })
